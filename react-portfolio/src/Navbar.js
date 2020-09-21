eef852909aedcc173763c0fc56874c32807fc799
Gabriel "fr4ct1ons" Lucena <gabriellucena23@gmail.com>
2020-09-21 17:51:29 -0300

    Created react app

---
+++ react-portfolio/src/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+
+class Navbar extends React.Component{
+render(){
+return(
+    <div className="Navbar">
+        <a href = "fr4ct1ons.myportfolio.com">FR4CT1ONS</a>
+    </div>
+)
+}
+}
+
+export default Navbar
