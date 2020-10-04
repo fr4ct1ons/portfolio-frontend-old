c6b6cbeb474d9ca7d5157066d0f1b050407909c3
Gabriel "fr4ct1ons" Lucena <gabriellucena23@gmail.com>
2020-10-04 11:44:13 -0300

    Created React portfolio

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,27 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div></div>
-        <Navbar/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+
+      <div style={{width: "70%"}, {textAlign: "center"}}>
+        <div style={{width: "66%"}, {margin: "auto"}, {display: "block"}}>
+        <h1>WHO AM I</h1>
+        <h2><i>Introducing who am I, what I work with and my goals</i></h2>
+        <p>Hey there! My name is Gabriel Lucena, also known as FR4CT1ONS.</p>
+        <p>I am an aspiring game programmer, artist and designer. Currently, I'm graduating in Information Technology at Universidade Federal do Rio Grande do Norte. 
+            Over there, I'm primarily studying game development and design along with software engineering. I am also studying arts as an auto-didact. 
+            After I graduate in I.T., I plan to stock up on money for taking a bachelor's degree course in arts/game arts.</p>
+        <p>I have already worked on multiple games with two W.I.P small projects already published on GameJolt and itch.io - Poço Frio and Single Fighter - and am developing a personal project more privately.</p>
+        <p>I have experience with C/C++, C# and with the Unity game engine. Currently, I'm studying the Unreal engine.</p>
+        <p>When it comes to arts, I have experience in 2D concept and final art, as well as 3D modelling in general. Currently, I'm studying animation.</p>
+        <br/>
+        <p>Right now, the public project I'm giving most of my attention to is Single Fighter. Go check it out! <a href="https://fr4ct1ons.itch.io/single-fighter">https://fr4ct1ons.itch.io/single-fighter</a></p>
+        </div>
+      </div>
     </div>
   );
 }
--- react-portfolio/src/Navbar.js
+++ react-portfolio/src/Navbar.js
@@ -1,15 +1,22 @@
 import React from 'react';
-//import logo from './logo.svg';
+import logo from './Logo final.svg';
 //import './App.css';
+import './cascades/Navbar.css';
 
 class Navbar extends React.Component{
+
 render(){
 return(
-    <div className="Navbar">
-        <a href = "fr4ct1ons.myportfolio.com">FR4CT1ONS</a>
+    <div className="Navbar" className="Navbar">
+        <ul className="Links">
+            <a href = "fr4ct1ons.myportfolio.com"> <img src = {logo} className = "Logo"/></a>
+            <li><a href="#">Test2</a></li>
+            <li><a href="#">Test1</a></li>
+        </ul>
     </div>
 )
 }
+
 }
 
 export default Navbar
