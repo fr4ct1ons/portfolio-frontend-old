f5c9cff3bb0500446a21f2e6fa0dfde6ca860119
Gabriel "fr4ct1ons" Lucena <gabriellucena23@gmail.com>
2020-12-08 23:27:03 -0300

    Added GalleryEntry

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 import Navbar from "./Navbar";
 import Page from "./Page";
 import { render } from 'react-dom';
-import ContactForm from "./ContactForm"
-
+import ContactForm from "./ContactForm";
+import GalleryEntry from "./GalleryEntry";
 
 class App extends React.Component{
   
@@ -68,12 +68,37 @@ render()
           </Route>
           <Route exact path="/games">
             <Page title = "GAMES" subtitle = "Games I worked on"></Page>
+            <GalleryEntry title="Single Fighter" 
+            img="https://raw.githubusercontent.com/lucena-fr4ct1ons/lucena-fr4ct1ons.github.io/master/games/games-assets/Single%20Fighter%20title.png"
+            url="https://fr4ct1ons.itch.io/single-fighter"></GalleryEntry>
+            <GalleryEntry title="Single Fighter2" 
+            img="https://raw.githubusercontent.com/lucena-fr4ct1ons/lucena-fr4ct1ons.github.io/master/games/games-assets/Single%20Fighter%20title.png"
+            url="https://fr4ct1ons.itch.io/single-fighter"></GalleryEntry>
+            <GalleryEntry title="Single Fighter3" 
+            img="https://raw.githubusercontent.com/lucena-fr4ct1ons/lucena-fr4ct1ons.github.io/master/games/games-assets/Single%20Fighter%20title.png"
+            url="https://fr4ct1ons.itch.io/single-fighter"></GalleryEntry>
+            <GalleryEntry title="Single Fighter444" 
+            img="https://raw.githubusercontent.com/lucena-fr4ct1ons/lucena-fr4ct1ons.github.io/master/games/games-assets/Single%20Fighter%20title.png"
+            url="https://fr4ct1ons.itch.io/single-fighter"></GalleryEntry>
+            <GalleryEntry title="Single Fighter444" 
+            img="https://raw.githubusercontent.com/lucena-fr4ct1ons/lucena-fr4ct1ons.github.io/master/games/games-assets/Single%20Fighter%20title.png"
+            url="https://fr4ct1ons.itch.io/single-fighter"></GalleryEntry>
           </Route>
           <Route exact path="/art">
             <Page title = "ART" subtitle = "Art pieces I made"></Page>
+            <GalleryEntry title="Workbench" 
+            img="https://pro2-bar-s3-cdn-cf6.myportfolio.com/2e727ec7-f6e9-4fd4-b97f-93196e1208ad/72bce5f9-2596-4820-b1b7-dd2a0bd7d964.jpg?h=9446e874cc19b1da82695f7fc0dc722c"
+            url="https://fr4ct1ons.myportfolio.com/inktober-2019"></GalleryEntry>
+            <GalleryEntry title="Unreal water shader"
+            img="https://pro2-bar-s3-cdn-cf5.myportfolio.com/2e727ec7-f6e9-4fd4-b97f-93196e1208ad/6f71a0c5-8cc3-4d7d-9b74-bf2a0bcc2a8d.png?h=f74f57784e5d1808d20b4f08dbc531de"
+            url="https://fr4ct1ons.myportfolio.com/stylized-unreal-ocean-water-shader"></GalleryEntry>
+
           </Route>
           <Route exact path="/software">
-            <Page title = "SOFTWARE" subtitle = "Software I develped"></Page>
+            <Page title = "SOFTWARE" subtitle = "Software I developed"></Page>
+            <GalleryEntry title="Unity Cel-Shaded Render Pipeline"
+            img="https://pro2-bar-s3-cdn-cf3.myportfolio.com/2e727ec7-f6e9-4fd4-b97f-93196e1208ad/485ffae0-7448-4fd9-915a-61f7192ede9f.png?h=2be02d12d0958faae66818c150bba371"
+            url="https://fr4ct1ons.myportfolio.com/cel-shaded-render-pipeline"></GalleryEntry>
           </Route>
         </Switch>
           
---
+++ react-portfolio/src/gallery/test.js
@@ -0,0 +1,3 @@
+const text = "Hello, world!"
+
+export default text;
